265b4de9bc91fe215959f703ee7a6f1c7a43ae5f
Mikhail Bely <neverdream40@gmail.com>
2022-02-04 20:38:49 +0300

    feat: update function reverse to right version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let revNum;
+    let revStr;
+    revNum = n.toString(10).split("");
+    if (revNum[0] == "-") {
+        revNum.splice(0, 1);
+    }
+    revStr = revNum.reverse().join("");
+    return parseInt(revStr);
+};
